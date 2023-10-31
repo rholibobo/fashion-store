2d65f3df8072eb7a4b31f13928747aa7fd8eabfe
rholibobo <rholibobo@gmail.com>
2023-10-31 15:18:11 +0100

    Set up thteming for client side

---
+++ client/src/utils/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+ palette: {
+   primary: {
+     main: "#fcba03",
+   },
+ },
+ typography: {
+    fontFamily: "inherit",
+  },
+});
+
+export default theme;
